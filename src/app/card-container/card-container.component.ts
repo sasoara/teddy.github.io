959960b84ad073d0a410e7e75b286acf1dc708c2
Sarah Bettinger <b.sarah2@hotmail.com>
2023-04-29 21:55:07 +0200

    moves cards into card-container component, adds functionality to store values

--- src/app/card-container/card-container.component.ts
+++ src/app/card-container/card-container.component.ts
@@ -41,6 +41,7 @@ export class CardContainerComponent implements OnInit {
     console.log(this.segmentIds);
   }
 
+  // WOMAN
   get woman() {
     return this.cardContainerForm.get('woman');
   }
@@ -52,6 +53,91 @@ export class CardContainerComponent implements OnInit {
   addWoman() {
     if (this.woman?.valid) {
       this.segmentIds.women.push(this.newWoman());
+    }
+    console.log(this.cardContainerForm.value);
+    this.cardContainerForm.reset();
+  }
+
+  // MAN
+  get man() {
+    return this.cardContainerForm.get('man');
+  }
+
+  newMan(): string {
+    return this.cardContainerForm.value.man!;
+  }
+
+  addMan() {
+    if (this.man?.valid) {
+      this.segmentIds.men.push(this.newMan());
+    }
+    console.log(this.cardContainerForm.value);
+    this.cardContainerForm.reset();
+  }
+
+  // DRESSW
+  get dressW() {
+    return this.cardContainerForm.get('dressW');
+  }
+
+  newDressW(): string {
+    return this.cardContainerForm.value.dressW!;
+  }
+
+  addDressW() {
+    if (this.dressW?.valid) {
+      this.segmentIds.dressesW.push(this.newDressW());
+    }
+    console.log(this.cardContainerForm.value);
+    this.cardContainerForm.reset();
+  }
+
+  // DRESSM
+  get dressM() {
+    return this.cardContainerForm.get('dressM');
+  }
+
+  newDressM(): string {
+    return this.cardContainerForm.value.dressM!;
+  }
+
+  addDressM() {
+    if (this.dressM?.valid) {
+      this.segmentIds.dressesM.push(this.newDressM());
+    }
+    console.log(this.cardContainerForm.value);
+    this.cardContainerForm.reset();
+  }
+
+  // TRANSPORT
+  get transport() {
+    return this.cardContainerForm.get('transport');
+  }
+
+  newTransport(): string {
+    return this.cardContainerForm.value.transport!;
+  }
+
+  addTransport() {
+    if (this.transport?.valid) {
+      this.segmentIds.transports.push(this.newTransport());
+    }
+    console.log(this.cardContainerForm.value);
+    this.cardContainerForm.reset();
+  }
+
+  // ACTIVITY
+  get activity() {
+    return this.cardContainerForm.get('activity');
+  }
+
+  newActivity(): string {
+    return this.cardContainerForm.value.activity!;
+  }
+
+  addActivity() {
+    if (this.activity?.valid) {
+      this.segmentIds.activities.push(this.newActivity());
     }
     console.log(this.cardContainerForm.value);
     this.cardContainerForm.reset();
