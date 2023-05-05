8a7cd148ea5ccad8698cbbfc90e9e636585da16d
Sarah Bettinger <b.sarah2@hotmail.com>
2023-05-05 23:15:49 +0200

    adds values to table dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, AfterViewInit, ViewChild} from '@angular/core';
 import {DynamicTableComponent} from "./dynamic-table/dynamic-table.component";
+import {DynamicTableService} from "./dynamic-table/dynamic-table.service";
 import {ITableData, ITableHeader} from "./dynamic-table/dynamic-table.models";
 
 @Component({
@@ -11,25 +12,19 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('dynamicTable') table!: DynamicTableComponent;
 
   title = 'DAS Spiel';
-  data = `[
-    {
-      "WOMAN": "Michelle Obama",
-      "MAN": "Donald Trump",
-      "DRESS WOMAN": "Sari",
-      "DRESS MAN": "Borat",
-      "TRANSPORT": "Traktor",
-      "ACTIVITY": "Broschüren über Zeugen Jehova verteilen"
-    }
-  ]`
+  tableData: ITableData[] = this._dynamicTableService.getTableData();
+
+  constructor(private _dynamicTableService: DynamicTableService) {
+  }
 
   ngAfterViewInit(): void {
     const headers = [
       'WOMAN', 'MAN', 'DRESS WOMAN',
       'DRESS MAN', 'TRANSPORT', 'ACTIVITY'
     ].map((x, i) => ({key: x, index: i, isSelected: true} as ITableHeader));
-    const data = JSON.parse(this.data) as ITableData[];
-    this.table.render(headers, data);
 
-    console.log('DATA:', data);
+    this.tableData = this._dynamicTableService.getTableData();
+    this.table.render(headers, this.tableData);
+    console.log('TABLEDATA', this.tableData);
   }
 }
